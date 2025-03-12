ddecfcb4ee4f7febc2d0a54e40d4efca34d57844
Maureen Nkirote <kinyuamoh81@gmail.com>
2025-03-12 23:11:31 +0300

    strings

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Maureen Zhu';
+
+const welcomeMessage = 'Welcome to Flatbook, + ${ Maureen Zhu }!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+'Maureen Zhu'.charAt(0)
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
+
